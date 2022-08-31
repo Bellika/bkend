b758b0c3471501d0558b8ce782a6af545edef7c7
Markus Bellika <markusbellika@gmail.com>
2022-08-31 19:40:27 +0200

    feat: Added docker

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express')
+const cron = require('node-cron')
 const app = express()
 
 const PORT = process.env.PORT || 3000
 
+cron.schedule('*/10 * * * * *', () => {
+  console.log('Running backend work')
+});
+
 app.listen(PORT, console.log('Listening on port 3000'))
+
 
 
